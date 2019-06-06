3092597ddd14450a4b0bdd545f855237076acc41
MyNameIsDabin <dorothy9795@gmail.com>
2019-06-06 13:37:45 +0900

    config.js 에 메뉴정보 추가

--- ionic4-vue-app/src/config/index.js
+++ ionic4-vue-app/src/config/index.js
@@ -1,5 +1,6 @@
 export const MENU_HOUSE = 0;
 export const MENU_HOTEL = 1;
+export const MENU_FITNESS_CENTER = 1;
 export const MENU_LIST = [{
     icon: 'home',
     text: '군 주택 특별공급',
@@ -8,4 +9,9 @@ export const MENU_LIST = [{
     icon: 'hotel',
     text: '호텔/콘도',
     idx: MENU_HOTEL
-  }];
+  },{
+    icon: 'dumbbell',
+    text: '체력단령장',
+    idx: MENU_FITNESS_CENTER
+  }
+];
